d7e4626052cf148ac1c82acb9e2c060aa6d7b286
junesdream <norisori@gmail.com>
2024-10-18 17:32:55 +0200

    Set up JobFactory and Seed Database with Dummy Data
    - Update CategoryFactory to generate random name and description for categories.
    - Update CompanyFactory to generate random name, description, location, industry, founded date, and website for companies.
    - Update JobFactory to generate random title, description, salary, location, company ID, category ID, and posted date for jobs.

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+
 
 class CompanySeeder extends Seeder
 {
@@ -12,6 +14,6 @@ class CompanySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Company::factory()->count(10)->create();
     }
 }
--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Job;
+
 
 class JobSeeder extends Seeder
 {
@@ -12,6 +14,8 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        {
+        Job::factory()->count(20)->create();
+        }
     }
 }
